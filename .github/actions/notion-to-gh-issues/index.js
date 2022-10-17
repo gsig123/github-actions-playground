a76f1deeb80ea93ec369350574289abc46ca8e8a
Bjartur Sigurbergsson <bjartur@hey.com>
2022-10-17 20:52:47 +0200

    Example for an upcoming article on GH actions
    * Custom action: `notion-to-gh-issues`
    * Workflow that runs a cron job at the end of each day

---
+++ .github/actions/notion-to-gh-issues/index.js
@@ -0,0 +1,65 @@
+const github = require("@actions/github");
+const core = require("@actions/core");
+
+const { Client } = require("@notionhq/client");
+
+const GITHUB_USERNAME = "gsig123";
+const GITHUB_REPO_NAME = "github-actions-playground";
+
+const notionPageTitleToIssueTitle = (pageTitle) => {
+  return `[Bug] ${pageTitle}`;
+};
+
+const issueTitleToNotionPageTitle = (issueTitle) => {
+  return issueTitle.split(`[Bug] `)[1];
+};
+
+async function run() {
+  const githubToken = core.getInput("githubToken");
+  const notionDbId = core.getInput("notionDbId");
+  const notionApiToken = core.getInput("notionApiToken");
+  const tagColumnName = core.getInput("tagColumnName");
+  const tagValue = core.getInput("tagValue");
+
+  const octokit = github.getOctokit(githubToken);
+  const notion = new Client({
+    auth: notionApiToken,
+  });
+
+  const newIssueCandidates = await notion.databases.query({
+    database_id: notionDbId,
+    filter: {
+      property: tagColumnName,
+      select: {
+        equals: tagValue,
+      },
+    },
+  });
+
+  const { data: issuesFromRepo } = await octokit.rest.issues.listForRepo({
+    owner: GITHUB_USERNAME,
+    repo: GITHUB_REPO_NAME,
+    per_page: 100, // Ignore pagination for now
+    state: "open",
+  });
+
+  const existingIssueIds = issuesFromRepo.map((issue) =>
+    issueTitleToNotionPageTitle(issue.title)
+  );
+
+  const bugsThatNeedIssuesToBeCreated = newIssueCandidates.filter(
+    ({ title }) => !existingIssueIds.includes(title)
+  );
+
+  bugsThatNeedIssuesToBeCreated.forEach((issueToBeCreated) => {
+    octokit.rest.issues.create({
+      owner: GITHUB_USERNAME,
+      repo: GITHUB_REPO_NAME,
+      title: notionPageTitleToIssueTitle(issueToBeCreated.id),
+      body: issueToBeCreated.url,
+      assignees: [GITHUB_USERNAME],
+    });
+  });
+}
+
+run();
